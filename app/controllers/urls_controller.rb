46029124802c330a798a34a46cf3afa27beb90ae
Ton Pham <tonpham35@gmail.com>
2017-05-25 15:24:03 +0800

    added url shortener

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -6,5 +6,24 @@ class UrlsController < ApplicationController
 	def show
     	@url = Url.find(params[:id])
 	end
+
+	def new
+  		@url = Url.new
+	end
+
+	def create
+  		@url = Url.new(url_params)
+  		@url.shorten
+  		if @url.save
+      		redirect_to @url  
+  		else
+      		render 'new'
+  		end   
+	end
+
+	private 
+	def url_params
+	    params.require(:url).permit(:long_url)
+	end
 end
 
