faea28be34422c3cb69d0aeb4fcfc0c7446cbf53
Ton Pham <tonpham35@gmail.com>
2017-05-25 14:04:46 +0800

    added url validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
 class Url < ApplicationRecord
+	validates_presence_of :long_url
+    validates_format_of :long_url, :with => URI::regexp(['http', 'https']),
+    :message => "Only letters allowed"
 end
